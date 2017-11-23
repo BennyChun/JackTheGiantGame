3f5bb11ce7d93b35ffc7fd4ecef7cf72c4e17594
Benny Chun <bennychun97@gmail.com>
2017-11-23 22:06:36 +1300

    character animation completed

---
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 8f, maxVelocity = 4f;
+
+	private Rigidbody2D myBody;
+	private Animator playerAnim;
+
+	void Awake() {
+		myBody = GetComponent<Rigidbody2D>();
+		playerAnim = GetComponent<Animator>();
+	}
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate() {
+		PlayerMoveKeyboard();
+	}
+
+	void PlayerMoveKeyboard() {
+		float forceX = 0f;
+		float vel = Mathf.Abs(myBody.velocity.x);
+
+		float h = Input.GetAxisRaw("Horizontal");
+
+		if (h > 0) {
+
+			if (vel < maxVelocity) {
+				forceX = speed;
+			}
+
+			Vector3 temp = transform.localScale;
+			playerAnim.SetBool("Walk", true);
+			temp.x = 1.3f;
+			transform.localScale = temp;
+			playerAnim.SetBool("Walk", true);
+
+		} else if (h < 0) {
+
+			if (vel < maxVelocity) {
+				forceX = -speed;
+			}
+			Vector3 temp = transform.localScale;
+			playerAnim.SetBool("Walk", true);
+			temp.x = -1.3f;
+			transform.localScale = temp;
+		} else {
+			playerAnim.SetBool("Walk", false);
+		}
+
+		myBody.AddForce(new Vector2(forceX, 0));
+
+	}
+
+}// Player
+
+
