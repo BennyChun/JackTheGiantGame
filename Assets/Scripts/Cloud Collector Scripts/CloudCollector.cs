1848432eab34f855c37dc09a298c36d667896e05
Benny Chun <bennychun97@gmail.com>
2017-11-23 23:20:08 +1300

    cloud spawner and cloud collector classes made, cloud collector implem done

---
+++ Assets/Scripts/Cloud Collector Scripts/CloudCollector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCollector : MonoBehaviour {
+
+	//the target is the cloud that collides with the collector
+	private void OnTriggerEnter2D(Collider2D target) {
+		if (target.tag == "Cloud" || target.tag == "Deadly") {
+			target.gameObject.SetActive(false);
+		}
+	}
+
+}
---
+++ Assets/Scripts/Cloud Collector Scripts/CloudSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] clouds;
+
+	// distance between y axis of the clouds
+	private float distanceBetweenClouds = 3f;
+
+	// to prevent clouds to go outside of camera
+	private float minX, maxX;
+
+	// to indicate we need to spawn more clouds
+	private float lastCloudPosY;
+
+	[SerializeField]
+	private GameObject[] collectables;
+
+	// to control x position of cloud so clouds dont spawn directly under another
+	private float controlX;
+
+	private GameObject player;
+
+	private void OnTriggerEnter2D(Collider2D target) {
+		if (target.tag == "Cloud" || target.tag == "Deadly") {
+			//target.gameObject.SetActive();
+		}
+	}
+
+}
